4da1d248c55f007151c698846039a26192d9b296
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-02 13:46:37 +0800

    feat: add a shortcut icon

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Noble Property Asia | Database Management System</title>
+        <link rel="shortcut icon" href="/assets/npa-logo.png" />
       </Head>
       <Component {...pageProps} />
     </>
