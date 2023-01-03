51398a2e4111336cf81f188a642314653d2dae2e
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-03 15:40:01 +0800

    feat: changes the default box shadow values of Tailwind CSS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,13 @@
 module.exports = {
   content: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
   theme: {
+    boxShadow: {
+      xs: "0px 1px 2px rgba(16, 24, 40, 0.05)",
+      sm: "0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06)",
+      md: "0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06)",
+      lg: "0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03)",
+      xl: "0px 20px 25px -5px rgba(16, 24, 40, 0.08), 0px 10px 10px -5px rgba(16, 24, 40, 0.03)",
+    },
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
