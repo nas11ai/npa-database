1e73c2ad7df761f9e8d50c71696e31547c30c74b
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-03 15:13:09 +0800

    feat: extends the default fontFamily prop
    Add an inter custom prop to extends the default fontFamily prop.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
