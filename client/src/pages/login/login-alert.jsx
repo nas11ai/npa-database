6f772a396fa5f09861a2e17c1874df52769f0b0c
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-07 13:51:05 +0800

    feat: add the login alert component

---
+++ client/src/pages/login/login-alert.jsx
@@ -0,0 +1,17 @@
+import classNames from "classnames";
+
+export const LoginAlert = ({ children, isShow }) => {
+  return (
+    <div
+      className={classNames(
+        "flex items-center gap-3",
+        isShow ? "block" : "hidden"
+      )}
+    >
+      <div className="h-4 w-4 rounded-full bg-red-500" />
+      <p className="text-sm text-red-500">{children}</p>
+    </div>
+  );
+};
+
+export default LoginAlert;
