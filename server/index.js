b1854ea2fbb179598b3ed6d450940037c5fae799
nas11ai <kairupannasai@gmail.com>
2023-01-12 18:53:49 +0800

    fix: add cookie-parser for reading cookies and refactor response message

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-// const cookieParser = require("cookie-parser");
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const cookieParser = require("cookie-parser");
 
 const {
-    registerRouter,
-    loginRouter,
-    refreshTokenRouter,
+  registerRouter,
+  loginRouter,
+  refreshTokenRouter,
 } = require("./controllers/users")
 
 const { PORT } = require("./utils/config")
@@ -16,19 +16,21 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("NA Database API is running 🥳")
+  res.send("NA Database API is running 🥳")
 })
 
-// app.use(cookieParser());
-app.use("/users/register", registerRouter)
-app.use("/users/login", loginRouter)
-app.use("/users/refresh_token", refreshTokenRouter)
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use('/users/register', registerRouter);
+app.use('/users/login', loginRouter);
+app.use('/users/refresh_token', refreshTokenRouter);
 
 const main = async () => {
-    await connectToDatabase()
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
 }
 
 main()
