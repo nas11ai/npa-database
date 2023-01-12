5020d85c98a94644546b1a8ca6c0146eced9ad91
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-12 10:04:08 +0800

    feat: create minimal functionality for redirect the user to the dashboard feature
    In the feature when the user accessing the web app, the web app will checks of the session before.
    
    If the user have a sessions before then redirect them to the dashboard page, if not then redirect them to the login page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <h1>Hello World </h1>;
 }
+
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/dashboard",
+      permanent: false,
+    },
+    props: {},
+  };
+};
