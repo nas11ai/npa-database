7d107df5773999393ebea33e3d3d09b7d2f9e5e2
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-12 19:24:49 +0800

    refactor: add a handleSidebarIsOpen prop
    The handleSidebarIsOpen will be used for handle toggle open and close the sidebar component.

--- client/src/pages/dashboard/header.jsx
+++ client/src/pages/dashboard/header.jsx
@@ -6,11 +6,11 @@ import {
 
 import { Avatar } from "@/components";
 
-export const Header = () => {
+export const Header = ({ handleSidebarIsOpen }) => {
   return (
-    <header className="flex items-center justify-between border-b-1 p-5 text-npa-neutral-400">
-      <button type="button">
-        <MdMenuOpen className="h-8 w-8 duration-200 hover:text-npa-neutral-500" />
+    <header className="sticky top-0 left-0 flex items-center justify-between border-b-1 bg-white p-3 px-5 text-npa-neutral-400 md:p-5">
+      <button type="button" onClick={() => handleSidebarIsOpen(true)}>
+        <MdMenuOpen className="h-8 w-8 rotate-180 duration-200 hover:text-npa-neutral-500" />
       </button>
 
       <div className="flex items-center gap-5">
