4398e93e6572f5ccf86fab88568b9883353a7402
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-12 19:30:11 +0800

    feat: create a state to store the current state of the open sidebar component
    This states will we passed to Header and Aside component

--- client/src/pages/dashboard/dashboard-layout.jsx
+++ client/src/pages/dashboard/dashboard-layout.jsx
@@ -1,13 +1,21 @@
+import { useState, useEffect } from "react";
+
 import { Aside } from "./aside";
 import { Header } from "./header";
 
 export const DashboardLayout = ({ children }) => {
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+
+  useEffect(() => {
+    setSidebarIsOpen(window.innerWidth >= 768);
+  }, []);
+
   return (
     <>
-      <Aside />
-      <div className="flex w-full flex-col">
-        <Header />
-        <main>{children}</main>
+      <Aside sidebarIsOpen={sidebarIsOpen} onSidebarIsOpen={setSidebarIsOpen} />
+      <div className="relative flex w-full flex-[2] flex-col">
+        <Header handleSidebarIsOpen={setSidebarIsOpen} />
+        <main className="h-screen overflow-auto p-5">{children}</main>
       </div>
     </>
   );
