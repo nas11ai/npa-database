efd48c7f410a3e3139924b401c852a57beb4aa34
Ersan Karimi <ersankarimii311@gmail.com>
2023-01-12 10:07:26 +0800

    feat: create the dashboard analytics route page

---
+++ client/src/pages/dashboard/analytics/index.jsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from "../dashboard-layout";
+
+export default function Analitycs() {
+  return (
+    <DashboardLayout>
+      <h1>Analitys</h1>
+    </DashboardLayout>
+  );
+}
