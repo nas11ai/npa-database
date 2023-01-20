17d6bbd0770411c2fd03eb4a462559366482d964
nas11ai <kairupannasai@gmail.com>
2023-01-20 20:31:38 +0800

    fix: commenting methods in cors

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const corsOptions = {
   origin: ['http://localhost:3000', 'https://na-database.vercel.app/', 'https://fe-na-database-bdilh31im-xploratech.vercel.app'],
 >>>>>>> e830b00 (feat: add more origin for cors policy)
   credentials: true,
-  methods: 'GET,POST,PUT,DELETE',
+  // methods: 'GET,POST,PUT,DELETE',
 }))
 app.use(express.json());
 app.use(cookieParser());
