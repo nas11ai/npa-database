eedf1838d2a0ca24e2380205de41348e6e4514e8
nas11ai <kairupannasai@gmail.com>
2023-01-20 22:15:51 +0800

    fix: cors policy error and cookie does not included in browser

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const { PORT } = require("./utils/config");
 const { connectToDatabase } = require("./utils/db");
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const corsOptions = {
   origin: [
     'http://localhost:3000',
