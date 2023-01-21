f72786e1bcc06d2853a216ccdc84f2ff748e0c42
nas11ai <kairupannasai@gmail.com>
2023-01-21 21:32:17 +0800

    feat add secure:true for cookie

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -22,6 +22,9 @@ router.post('/', async (req, res, next) => {
       //TODO: Ganti ke 1 hari kalau deployment
       // maxAge: 24 * 60 * 60 * 1000,
       maxAge: 15 * 1000,
+=======
+      maxAge: 24 * 60 * 60 * 1000,
+>>>>>>> aa7f850 (feat add secure:true for cookie)
     });
 
     const response = new SuccessResponse(200, "OK", new DataDetails("login", {
