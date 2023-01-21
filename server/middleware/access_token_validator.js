a75652759af45c4364953ddbd8063fb751e4f57f
nas11ai <kairupannasai@gmail.com>
2023-01-21 14:27:59 +0800

    fix: login and auth logic error

--- server/middleware/access_token_validator.js
+++ server/middleware/access_token_validator.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken');
+<<<<<<< HEAD
 
 const { ErrorResponse, ErrorDetails } = require("../../models/response");
+=======
+>>>>>>> 8325ee2 (fix: login and auth logic error)
 const {
   ACCESS_TOKEN_SECRET,
   TOKEN_ALGORITHM,
@@ -12,6 +15,7 @@ const accessTokenValidator = (req, res, next) => {
   try {
     const authorizationHeader = req.header('authorization');
     if (!authorizationHeader) {
+<<<<<<< HEAD
       const err = new ErrorDetails("AccessTokenError", "access_token", "is wrong");
       // TODO: ganti console ke log kalau sudah mau production
       console.error(err);
@@ -46,6 +50,48 @@ const accessTokenValidator = (req, res, next) => {
     // TODO: ganti console ke log kalau sudah mau production
     console.error(err);
     throw new ErrorResponse(401, "UNAUTHORIZED", { [err.attribute]: err.message });
+=======
+      res.status(401).json({
+        error: true,
+        name: "AccessTokenError",
+        message: "Invalid token",
+      });
+      return
+    }
+
+    const parts = authorizationHeader.split(' ');
+    if (parts.length !== 2 || parts[0].toLowerCase() !== 'bearer') {
+      res.status(401).json({
+        error: true,
+        name: "AccessTokenError",
+        message: "Missing access token",
+      });
+      return;
+    }
+
+    const accessToken = parts[1];
+
+    jwt.verify(accessToken, ACCESS_TOKEN_SECRET, {
+      algorithm: TOKEN_ALGORITHM,
+      issuer: TOKEN_ISSUER,
+      audience: TOKEN_AUDIENCE,
+    });
+
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      res.status(401).json({
+        error: true,
+        name: "AccessTokenError",
+        message: "Access token has expired",
+      });
+    }
+    res.status(500).json({
+      error: true,
+      name: "AccessTokenError",
+      message: error.message,
+    });
+>>>>>>> 8325ee2 (fix: login and auth logic error)
   }
 }
 
