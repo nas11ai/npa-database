afc2fef53154ee2cc9b7dee2bed804256bdb398a
nas11ai <kairupannasai@gmail.com>
2023-01-21 17:27:18 +0800

    feat: add more origin for cors policy

--- server/index.js
+++ server/index.js
@@ -16,8 +16,6 @@ const {
 const { PORT } = require("./utils/config");
 const { connectToDatabase } = require("./utils/db");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 const corsOptions = {
   origin: [
     'http://localhost:3000',
