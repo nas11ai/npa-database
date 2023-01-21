7af7eb18cc6296df3003658647876c6181dda70b
nas11ai <kairupannasai@gmail.com>
2023-01-21 18:53:06 +0800

    feat: using self made middleware for cors policy

--- server/index.js
+++ server/index.js
@@ -52,7 +52,15 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
