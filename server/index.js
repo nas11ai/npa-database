f41cd0b58505cdc67fd8ba51587d72f5c90b0c14
nas11ai <kairupannasai@gmail.com>
2023-01-21 21:20:45 +0800

    feat: remove secure:true for development

--- server/index.js
+++ server/index.js
@@ -22,6 +22,9 @@ const {
 const { PORT } = require("./utils/config");
 const { connectToDatabase } = require("./utils/db");
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 const corsOptions = {
   origin: [
     'http://localhost:3000',
