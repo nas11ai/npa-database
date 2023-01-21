1c55e3e8aaf7903b7fbc0aec3f806bfddf52e56e
nas11ai <kairupannasai@gmail.com>
2023-01-21 23:01:36 +0800

    feat: change cookie expired time to 10 second

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -31,9 +31,14 @@ router.post('/', async (req, res, next) => {
     res.cookie('jwt', newRefreshToken, {
       httpOnly: true,
       sameSite: 'None',
-      secure: false,
+      secure: true,
+<<<<<<< HEAD
       maxAge: 24 * 60 * 60 * 1000,
 >>>>>>> 7c3571e (feat: remove secure:true for development)
+=======
+      // maxAge: 24 * 60 * 60 * 1000,
+      maxAge: 10 * 1000,
+>>>>>>> eb5e7af (feat: change cookie expired time to 10 second)
     });
 
     const response = new SuccessResponse(200, "OK", new DataDetails("login", {
