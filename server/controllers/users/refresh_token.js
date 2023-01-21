acf4a8a9a455fff551bd87a893c5d6fe44a330eb
nas11ai <kairupannasai@gmail.com>
2023-01-21 21:20:45 +0800

    feat: remove secure:true for development

--- server/controllers/users/refresh_token.js
+++ server/controllers/users/refresh_token.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { refreshTokenValidator } = require("../../services/users");
 
+<<<<<<< HEAD
 const { SuccessResponse, DataDetails, ErrorResponse, ErrorDetails } = require("../../models/response");
 
 router.get("/", async (req, res, next) => {
@@ -56,6 +57,16 @@ router.post("/", async (req, res, next) => {
       // TODO: ganti console ke log kalau sudah mau production
       console.error(err);
       throw new ErrorResponse(401, "UNAUTHORIZED", { [err.attribute]: err.message });
+=======
+router.get("/", async (req, res, next) => {
+  try {
+    console.log(req.cookies);
+
+    const cookies = req.cookies;
+
+    if (!cookies?.jwt) {
+      throw new RefreshTokenError(401, "Missing refresh token");
+>>>>>>> 7c3571e (feat: remove secure:true for development)
     }
 
     const refreshToken = cookies.jwt
