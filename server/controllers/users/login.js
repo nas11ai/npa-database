dbab25d2bba47ac9382615c4a5ab69a33f9d6ad7
nas11ai <kairupannasai@gmail.com>
2023-01-21 23:34:16 +0800

    feat: change token expired time for development

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -9,7 +9,6 @@ router.post('/', async (req, res, next) => {
 
     const { newAccessToken, newRefreshToken, userRole } = await login(username, password);
 
-<<<<<<< HEAD
     res.cookie('jwt', newRefreshToken, {
       httpOnly: true,
       sameSite: 'None',
@@ -17,26 +16,6 @@ router.post('/', async (req, res, next) => {
       //TODO: Ganti ke 1 hari kalau deployment
       // maxAge: 24 * 60 * 60 * 1000,
       maxAge: 15 * 1000,
-=======
-    // if (error) {
-    //   res.status(error.statusCode).json({
-    //     error: true,
-    //     name: error.name,
-    //     message: error.message,
-    //   });
-    //   return;
-    // }
-
-    res.cookie('jwt', newRefreshToken, {
-      httpOnly: true,
-      sameSite: 'None',
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000,
->>>>>>> 7c3571e (feat: remove secure:true for development)
-=======
-      // maxAge: 24 * 60 * 60 * 1000,
-      maxAge: 10 * 1000,
->>>>>>> eb5e7af (feat: change cookie expired time to 10 second)
     });
 
     const response = new SuccessResponse(200, "OK", new DataDetails("bearer_token", {
