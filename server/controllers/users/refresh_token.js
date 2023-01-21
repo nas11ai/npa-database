37fb5ed8b7ceee32289f93492a9332074f33bc2f
nas11ai <kairupannasai@gmail.com>
2023-01-21 21:20:45 +0800

    feat: remove secure:true for development

--- server/controllers/users/refresh_token.js
+++ server/controllers/users/refresh_token.js
@@ -28,7 +28,11 @@ router.get("/", async (req, res, next) => {
       access_token: newAccessToken,
     });
   } catch (error) {
+<<<<<<< HEAD
     res.clearCookie('jwt');
+=======
+    // res.clearCookie('refresh_token');
+>>>>>>> 7c3571e (feat: remove secure:true for development)
     next(error);
   }
 =======
