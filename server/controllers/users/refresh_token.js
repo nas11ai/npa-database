4c051a075bfa1ba1767e004ee8f957254a4c8380
nas11ai <kairupannasai@gmail.com>
2023-01-21 22:38:53 +0800

    feat: add trust proxy

--- server/controllers/users/refresh_token.js
+++ server/controllers/users/refresh_token.js
@@ -1,50 +1,52 @@
-const router = require('express').Router();
-const { refreshTokenValidator } = require("../../services/users");
+const jwt = require('jsonwebtoken');
 
-<<<<<<< HEAD
-const { SuccessResponse, DataDetails, ErrorResponse, ErrorDetails } = require("../../models/response");
+const { ErrorResponse, ErrorDetails } = require("../models/response");
+const {
+  ACCESS_TOKEN_SECRET,
+  TOKEN_ALGORITHM,
+  TOKEN_ISSUER,
+  TOKEN_AUDIENCE,
+} = require("../utils/config");
 
-router.get("/", async (req, res, next) => {
+const accessTokenValidator = (req, res, next) => {
+  const authorizationHeader = req.header('authorization');
+  if (!authorizationHeader) {
+    const err = new ErrorDetails("AccessTokenError", "access_token", "access token is wrong");
+    // TODO: ganti console ke log kalau sudah mau production
+    console.error(err);
+    throw new ErrorResponse(401, "UNAUTHORIZED", { [err.attribute]: err.message });
+  }
+
+  const parts = authorizationHeader.split(' ');
+  if (parts.length !== 2 || parts[0].toLowerCase() !== 'bearer') {
+    const err = new ErrorDetails("AccessTokenError", "access_token", "access token is missing");
+    // TODO: ganti console ke log kalau sudah mau production
+    console.error(err);
+    throw new ErrorResponse(401, "UNAUTHORIZED", { [err.attribute]: err.message });
+  }
+
+  const accessToken = parts[1];
+
   try {
-    console.log(req.cookies);
+    jwt.verify(accessToken, ACCESS_TOKEN_SECRET, {
+      algorithm: TOKEN_ALGORITHM,
+      issuer: TOKEN_ISSUER,
+      audience: TOKEN_AUDIENCE,
+    });
 
-    const cookies = req.cookies;
-
-    if (!cookies?.jwt) {
-      const err = new ErrorDetails("BlacklistTokenError", "refresh_token", "is missing");
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      const err = new ErrorDetails("AccessTokenError", "access_token", "access token has expired");
       // TODO: ganti console ke log kalau sudah mau production
       console.error(err);
       throw new ErrorResponse(401, "UNAUTHORIZED", { [err.attribute]: err.message });
-=======
-router.get("/", async (req, res, next) => {
-  try {
-    console.log(req.cookies);
-
-    const cookies = req.cookies;
-
-    if (!cookies?.jwt) {
-      throw new RefreshTokenError(401, "Missing refresh token");
->>>>>>> 7c3571e (feat: remove secure:true for development)
     }
-
-    const refreshToken = cookies.jwt
-
-    const { newAccessToken, userRole } = await refreshTokenValidator(refreshToken);
-
-    res.status(201).json({
-      error: false,
-      message: "New access token has been created",
-      user_role: userRole,
-      access_token: newAccessToken,
-    });
-  } catch (error) {
-<<<<<<< HEAD
-    res.clearCookie('jwt');
-=======
-    // res.clearCookie('refresh_token');
->>>>>>> 7c3571e (feat: remove secure:true for development)
-    next(error);
+    const err = new ErrorDetails("AccessTokenError", "access_token", error.message);
+    // TODO: ganti console ke log kalau sudah mau production
+    console.error(err);
+    throw new ErrorResponse(401, "UNAUTHORIZED", { [err.attribute]: err.message });
   }
-});
+}
 
-module.exports = router;
+module.exports = accessTokenValidator
