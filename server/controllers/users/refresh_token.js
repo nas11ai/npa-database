3c25d3f5b7b1a902fe45655b52cad2141ccfff32
nas11ai <kairupannasai@gmail.com>
2023-01-21 22:38:53 +0800

    feat: add trust proxy

--- server/controllers/users/refresh_token.js
+++ server/controllers/users/refresh_token.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { refreshTokenValidator } = require("../../services/users");
 
-<<<<<<< HEAD
 const { SuccessResponse, DataDetails, ErrorResponse, ErrorDetails } = require("../../models/response");
 
 router.get("/", async (req, res, next) => {
@@ -28,11 +27,7 @@ router.get("/", async (req, res, next) => {
       access_token: newAccessToken,
     });
   } catch (error) {
-<<<<<<< HEAD
     res.clearCookie('jwt');
-=======
-    // res.clearCookie('refresh_token');
->>>>>>> 7c3571e (feat: remove secure:true for development)
     next(error);
   }
 });
