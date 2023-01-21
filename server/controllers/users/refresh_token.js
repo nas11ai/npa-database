297be75511a9dbe2842594f2c70940afb95ae1d7
nas11ai <kairupannasai@gmail.com>
2023-01-21 21:20:45 +0800

    feat: remove secure:true for development

--- server/controllers/users/refresh_token.js
+++ server/controllers/users/refresh_token.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { refreshTokenValidator } = require("../../services/users");
 
+<<<<<<< HEAD
 const { SuccessResponse, DataDetails, ErrorResponse, ErrorDetails } = require("../../models/response");
 
 router.get("/", async (req, res, next) => {
@@ -10,7 +11,20 @@ router.get("/", async (req, res, next) => {
     const cookies = req.cookies;
 
     if (!cookies?.jwt) {
+      const err = new ErrorDetails("BlacklistTokenError", "refresh_token", "is missing");
+      // TODO: ganti console ke log kalau sudah mau production
+      console.error(err);
+      throw new ErrorResponse(401, "UNAUTHORIZED", { [err.attribute]: err.message });
+=======
+router.get("/", async (req, res, next) => {
+  try {
+    console.log(req.cookies);
+
+    const cookies = req.cookies;
+
+    if (!cookies?.jwt) {
       throw new RefreshTokenError(401, "Missing refresh token");
+>>>>>>> 7c3571e (feat: remove secure:true for development)
     }
 
     const refreshToken = cookies.jwt
@@ -24,7 +38,11 @@ router.get("/", async (req, res, next) => {
       access_token: newAccessToken,
     });
   } catch (error) {
+<<<<<<< HEAD
     res.clearCookie('jwt');
+=======
+    // res.clearCookie('refresh_token');
+>>>>>>> 7c3571e (feat: remove secure:true for development)
     next(error);
   }
 });
