9ef92a87b1cb1113aab8df5b91c97113c1f10b05
nas11ai <kairupannasai@gmail.com>
2023-01-21 23:01:36 +0800

    feat: change cookie expired time to 10 second

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -13,34 +13,16 @@ router.post('/', async (req, res, next) => {
     httpOnly: true,
     sameSite: 'None',
     secure: true,
-    //TODO: Ganti ke 1 hari kalau deployment
     // maxAge: 24 * 60 * 60 * 1000,
-    maxAge: 15 * 1000,
+    maxAge: 10 * 1000,
   });
 
-  const response = new SuccessResponse(200, "OK", new DataDetails("login", {
-    "user_role": userRole,
-    "access_token": newAccessToken,
-  }));
-
-  res.status(response.code).json(response);
-} catch (error) {
-  next(error);
-}
-
-res.cookie('jwt', newRefreshToken, {
-  httpOnly: true,
-  sameSite: 'None',
-  secure: false,
-  maxAge: 24 * 60 * 60 * 1000,
-});
-
-res.status(200).json({
-  error: false,
-  message: "Login success",
-  user_role: userRole,
-  access_token: newAccessToken
-});
+  res.status(200).json({
+    error: false,
+    message: "Login success",
+    user_role: userRole,
+    access_token: newAccessToken
+  });
 });
 
 module.exports = router;
