e2a6187a29a5e4c1b80de197debe1e7c110ef491
nas11ai <kairupannasai@gmail.com>
2023-01-23 12:37:24 +0800

    refactor: update response message

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -4,25 +4,30 @@ const { login } = require("../../services/users");
 
 
 router.post('/', async (req, res, next) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  const { newAccessToken, newRefreshToken, userRole, error } = await login(username, password);
+    const { newAccessToken, newRefreshToken, userRole, error } = await login(username, password);
 
-  res.cookie('jwt', newRefreshToken, {
-    path: "/",
-    httpOnly: true,
-    sameSite: 'None',
-    secure: true,
-    // maxAge: 24 * 60 * 60 * 1000,
-    maxAge: 15 * 1000,
-  });
+    res.cookie('jwt', newRefreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+      //TODO: Ganti ke satu hari kalau sudah mau production
+      // maxAge: 24 * 60 * 60 * 1000,
+      maxAge: 15 * 1000,
+    });
 
-  res.status(200).json({
-    error: false,
-    message: "Login success",
-    user_role: userRole,
-    access_token: newAccessToken
-  });
+    const response = new SuccessResponse(200, "OK", new DataDetails("bearer_token", {
+      "user_role": userRole,
+      "access_token": newAccessToken,
+    }));
+
+    res.status(response.code).json(response);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
