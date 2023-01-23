0c2a86c84d9878d9be4e2ee471ac152ea7800869
nas11ai <kairupannasai@gmail.com>
2023-01-23 13:11:24 +0800

    fix: merge resolve bug

--- server/index.js
+++ server/index.js
@@ -16,9 +16,6 @@ const {
 const { PORT } = require("./utils/config");
 const { connectToDatabase } = require("./utils/db");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const corsOptions = {
   origin: [
     'http://localhost:3000',
