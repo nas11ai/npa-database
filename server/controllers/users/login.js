5ef41ddcb416f98c487072c661328bac57918aba
nas11ai <kairupannasai@gmail.com>
2023-01-23 12:37:24 +0800

    refactor: update response message

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { SuccessResponse, DataDetails } = require('../../models/response');
 const { login } = require("../../services/users");
 router.post('/', async (req, res, next) => {
-<<<<<<< HEAD
   try {
     const { username, password } = req.body;
 
@@ -28,23 +27,10 @@ router.post('/', async (req, res, next) => {
     //   return;
     // }
 
-=======
->>>>>>> 16ef581 (feat: change token expired time for development)
-      res.cookie('jwt', newRefreshToken, {
-        httpOnly: true,
-        sameSite: 'None',
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000,
->>>>>>> 7c3571e (feat: remove secure:true for development)
-=======
-      // maxAge: 24 * 60 * 60 * 1000,
-      maxAge: 10 * 1000,
->>>>>>> eb5e7af (feat: change cookie expired time to 10 second)
-=======
-      // maxAge: 24 * 60 * 60 * 1000,
-      maxAge: 15 * 1000,
->>>>>>> 16ef581 (feat: change token expired time for development)
-      });
+    const response = new SuccessResponse(200, "OK", new DataDetails("login", {
+      "user_role": userRole,
+      "access_token": newAccessToken,
+    }));
 
       const response = new SuccessResponse(200, "OK", new DataDetails("login", {
         "user_role": userRole,
