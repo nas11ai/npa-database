bea10a9e03f60e9c73ef49040711149b5ba4fc65
nas11ai <kairupannasai@gmail.com>
2023-01-26 19:55:33 +0800

    feat: fix merge resolve bug

--- server/index.js
+++ server/index.js
@@ -12,25 +12,29 @@ const {
   logoutRouter,
 } = require("./controllers/users");
 
-//TODO: add allowed origins
+const {
+  createNewPropertyAreaRouter,
+  readPropertyAreaRouter,
+  updatePropertyAreaRouter,
+  deletePropertyAreaRouter
+} = require("./controllers/property/areas");
+
 const { PORT } = require("./utils/config");
 const { connectToDatabase } = require("./utils/db");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://na-database.vercel.app',
+    new RegExp('https:\/\/fe-na-database-.*-xploratech\.vercel\.app'),
+    'https://fe-na-database-k1czrpt89-xploratech.vercel.app',
+  ],
   credentials: true,
 };
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
-  next();
-});
+app.set("trust proxy", 1);
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
